9622a6c723ce498d0fd024995e1dcdef997e9a61
Kishor Ghising <kghisinga@gmail.com>
2022-02-19 22:08:10 +0545

    Switch to react history for redirect to previous page

--- src/projectPages/LaticreteDesign.jsx
+++ src/projectPages/LaticreteDesign.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter, Link, useHistory } from 'react-router-dom';
 
 import {
   PROJECT_URL
@@ -11,22 +11,26 @@ import { faArrowAltCircleLeft } from '@fortawesome/free-regular-svg-icons';
 
 import laticreteDesign from '../Images/laticrete/laticreteDesign.png';
 
-class ProjectDetails extends React.Component {
-  render() {
-    const style = {
-      backgroundImage: `url(${laticreteDesign})`,
-    }
-    return (
-      <section>
-        <a href={PROJECT_URL} className="back-button">
-          <FontAwesomeIcon className="left-icon" icon={faArrowAltCircleLeft} size="md" />
-          <label htmlFor="back-icon">Back</label>
-        </a>
-        <div className='project-details' style={style}>
-        </div>
-      </section>
-    );
+const ProjectDetails = () =>   {
+  const history = useHistory();
+
+  function handleClick() {
+    history.push("/projects");
   }
+
+  const style = {
+    backgroundImage: `url(${laticreteDesign})`,
+  }
+  return (
+    <section>
+      <a onClick={handleClick} className="back-button">
+        <FontAwesomeIcon className="left-icon" icon={faArrowAltCircleLeft} size="md" />
+        <label htmlFor="back-icon">Back</label>
+      </a>
+      <div className='project-details' style={style}>
+      </div>
+    </section>
+  );
 }
 
 ProjectDetails.propTypes = {
