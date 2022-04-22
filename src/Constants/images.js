1059634f0f39e7195ec8415dd21e64a787dd707e
Kishor Ghising <kghisinga@gmail.com>
2022-04-22 08:46:19 +0545

    Add pioneer thumbnail

--- src/Constants/images.js
+++ src/Constants/images.js
@@ -1,26 +1,30 @@
 import chasmaghar from '../Images/chasmaghar/chasmagharThumbnail.png';
 import mirasacademy from '../Images/mirasacademy/mirasacademyThumbnail.png';
 import laticrete from '../Images/laticrete/laticreteThumbnail.png';
+import pioneer from '../Images/pioneer/pioneerThumbnail.png';
 
 export const projects = [
     {
+        name: "Pioneer Adventure",
+        type: "UX Design",
+        thumbnailPath: pioneer,
+        detailpageUrl: "/portfolio/projects/chasmaghar",
+    },
+    {
         name: "Chasmaghar",
         type: "UX Design",
-        designPath: "../Images/chasmaghar/chasmagharDesign.png",
         thumbnailPath: chasmaghar,
         detailpageUrl: "/portfolio/projects/chasmaghar",
     },
     {
         name: "Miras Academy",
         type: "UX Design",
-        designPath: "../Images/mirasacademy/mirasacademyDesign.png",
         thumbnailPath: mirasacademy,
         detailpageUrl: "/portfolio/projects/mirasacademy",
     },
     {
         name: "Laticrete",
         type: "UI Design",
-        designPath: "../Images/laticrete/laticreteDesign.png",
         thumbnailPath: laticrete,
         detailpageUrl: "/portfolio/projects/laticrete",
     },
